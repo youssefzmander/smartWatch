0edeb89b64b6c480464f3fddcc92fb5968aadbc9
youssefzmander <73340178+youssefzmander@users.noreply.github.com>
2023-02-27 23:37:48 +0100

    UI

---
+++ app/src/main/java/com/example/smartwatch/menu.kt
@@ -0,0 +1,11 @@
+package com.example.smartwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
